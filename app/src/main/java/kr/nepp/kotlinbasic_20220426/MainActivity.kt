ae3819009c502ba6cf509a35291d902e646b4650
조경진 <cho881020@gmail.com>
2022-04-26 14:18:10 +0900

    변수 자료형 설정 관련 예시

--- app/src/main/java/kr/nepp/kotlinbasic_20220426/MainActivity.kt
+++ app/src/main/java/kr/nepp/kotlinbasic_20220426/MainActivity.kt
@@ -27,6 +27,27 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        btnVariableTest.setOnClickListener {
+
+//            변수 문법 연습 코드
+
+//            강사 신상 정보 변수에 저장
+
+//            var myName : String  // variable : 변수 => 다른 값 변경 가능
+//            val myBirthYear : Int // value : 상수 => 한번 대입하면 변경 불가.
+//
+//            myName = "조경진"
+//            myBirthYear = 1988
+//
+//            myName = "이경진"
+//            myBirthYear = 1977
+
+            val myName = "조경진" // 자료형 자동 유추 설정.
+            val myBirthYear = 1988 // Int로 자동 설정
+            val myHeight : Double = 185.5 // Double 수동 설정 예시
+
+        }
+
     }
 
 }
