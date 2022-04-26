e5e21fdbecd543926daeec2ff8e905ce9f3adb5a
조경진 <cho881020@gmail.com>
2022-04-26 14:05:34 +0900

    코틀린 확장팩 플러그인 설치  - xml에 id만 붙이면, 코틀린에서 끌어다 사용 가능

--- app/src/main/java/kr/nepp/kotlinbasic_20220426/MainActivity.kt
+++ app/src/main/java/kr/nepp/kotlinbasic_20220426/MainActivity.kt
@@ -2,10 +2,16 @@ package kr.nepp.kotlinbasic_20220426
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
 }
