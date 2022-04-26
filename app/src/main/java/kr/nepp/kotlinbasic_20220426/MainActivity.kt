afb13f4754a8158302ff6706b179c73174087129
조경진 <cho881020@gmail.com>
2022-04-26 14:11:12 +0900

    로그 / 토스트 띄워보기 (이벤트 처리)

--- app/src/main/java/kr/nepp/kotlinbasic_20220426/MainActivity.kt
+++ app/src/main/java/kr/nepp/kotlinbasic_20220426/MainActivity.kt
@@ -2,7 +2,9 @@ package kr.nepp.kotlinbasic_20220426
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +13,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnLog.setOnClickListener {
+
+            Log.d("메인화면",  "로그 버튼 눌림!")
+            Log.e("메인화면", "에러 로그 테스트")
+
+        }
+
+        btnToast.setOnClickListener {
+
+            Toast.makeText(this, "토스트 띄우기", Toast.LENGTH_SHORT).show()
+
+
+        }
 
     }
 
